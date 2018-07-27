dae35f927c58cd3e4b3c0ff3903e6f929f16f6d7
NishantSingh07 <nis.kr.singh@gmail.com>
2018-07-27 16:42:51 +0530

    Rest

--- src/main/java/TestBase/TestBase.java
+++ src/main/java/TestBase/TestBase.java
@@ -5,17 +5,17 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
-import org.testng.annotations.Test;
 
 public class TestBase {
 	
-	@Test
-	public void ReadProp() throws IOException {
+
+	public  void ReadProp() throws IOException {
 		Properties prop = new Properties();
 		
 		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\test.properties");
 		prop.load(fis);
 		System.out.println(prop.getProperty("Name"));
+		return;
 				
 	}
 	
---
+++ src/test/java/TestScripts/TestRestClient.java
@@ -0,0 +1,19 @@
+package TestScripts;
+
+import org.testng.annotations.Test;
+
+import TestBase.TestBase;
+
+public class TestRestClient extends TestBase{
+	TestBase tb;
+	
+	
+
+	
+	@Test
+	public TestRestClient()
+	{
+		
+		
+	}
+}
